2da8b3ada3d4048fc88f90625bdc49ffc4ae87f6
Zate <zate75@gmail.com>
2011-03-20 22:22:20 -0400

    user stuff done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,20 @@
 # == Schema Information
-# Schema version: 20110318200855
+# Schema version: 20110321014252
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor :password
+	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
@@ -21,4 +24,37 @@ class User < ActiveRecord::Base
 	validates :email, 	:presence 	=> true,
 				:format		=> { :with => email_regex },
 				:uniqueness     => { :case_ensitive => false }
+	validates :password,	:presence	=> true,
+				:confirmation	=> true,
+				:length		=> { :within => 8..56 }
+	before_save :encrypt_password
+
+	def has_password?(submitted_password)
+		encrypted_password == encypt(sumitted_password)
+	end
+
+	def self.authenticate(email, submitted_password)
+		user = find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
+	end
+
+	private
+
+		def encrypt_password
+			self.salt = make_salt if new_record?
+			self.encrypted_password = encrypt(password)
+		end
+		
+		def encrypt(string)
+			secure_hash("#{salt}--#{string}")
+		end
+
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110320223537) do
+ActiveRecord::Schema.define(:version => 20110321014252) do
 
   create_table "books", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(:version => 20110320223537) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
